68efc7b4246181896542e7cde639e5acd6420fd6
Endrew Alves <tecnologia7@isla.com.br>
2025-04-16 16:32:04 -0300

    Gerado funções em arquivos .js

---
+++ cap2/ex2_1.js
@@ -0,0 +1,6 @@
+function mostrarOla() {
+    let nome = document.getElementById('nome').value;
+    document.getElementById('resposta').textContent = "Olá " + nome;
+}
+let mostrar = document.getElementById('mostrar');
+mostrar.onclick = mostrarOla;
